0ad1d2fdf09d7ad22aa8847172513266a42d59c6
sunnyyeti <sunnyyetizh@gmail.com>
2018-06-22 01:42:22 +0200

    lele

--- scripts/Siamese_CNN_v1/data.py
+++ scripts/Siamese_CNN_v1/data.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import pickle
+#import pickle
 import hashlib
 sys.path.append("../")
 import numpy as np
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 import torch
+import cPickle as pickle
 from feature_engineering.util import DataSet
 _PAD_ = "_PAD_"
 class GreedyBucket(object):
@@ -100,16 +101,191 @@ class GreedyBucket(object):
         return buckets,bounds
 
 class PaddedTensorDataset(Dataset):
-    def __init__(self, data_tensor, target_tensor):
-        assert len(data_tensor)==len(target_tensor)
-        self.data_tensor = torch.LongTensor(data_tensor)
-        self.target_tensor = torch.LongTensor(target_tensor)
+    def __init__(self, *data_tensors):
+        #self.data_tensors = [torch.LongTensor(data) for data in data_tensors]
+        self.data_tensors = data_tensors
+        lens = np.array([len(data) for data in data_tensors])
+        assert not np.any(lens-lens[0])
 
     def __getitem__(self, index):
-        return self.data_tensor[index], self.target_tensor[index]
+        return tuple([datatensor[index] for datatensor in self.data_tensors])
+
+    def __len__(self):
+        return len(self.data_tensors[0])
+
+class PaddedTensorDatasetTest(Dataset):
+    def __init__(self, data,ind):
+        #self.data_tensors = [torch.LongTensor(data) for data in data_tensors]
+        self.data_tensor = data
+        self.ind = ind
+        assert len(data)==len(ind)
+
+    def __getitem__(self, index):
+        return self.data_tensor[index],self.ind[index]
 
     def __len__(self):
         return len(self.data_tensor)
+
+class PaddedTensorDatasetNoTest(Dataset):
+    def __init__(self, data,ind,label):
+        #self.data_tensors = [torch.LongTensor(data) for data in data_tensors]
+        self.data_tensor = data
+        self.ind = ind
+        self.label = label
+        assert len(data)==len(ind)==len(label)
+
+    def __getitem__(self, index):
+        return self.data_tensor[index],self.ind[index],self.label[index]
+
+    def __len__(self):
+        return len(self.data_tensor)
+
+
+class DataGenerator(object):
+    def __init__(self):
+        if os.path.exists(self._temp_file):
+            print("detect cached intermediate files...loading...")
+            all_cached = pickle.load(open(self._temp_file,"rb"))
+            self.item2idx = all_cached["item2idx"]
+            self.idx2item = all_cached["idx2item"]
+            self.item_embed = all_cached["item_embed"]
+            self.q2idvec = all_cached["q2idvec"]
+            print("finish")
+        else:
+            print("Generating intermediate files...")
+            self.item2idx = {}
+            self.idx2item = {}
+            self.item_embed = {}
+            self.q2idvec = {}
+            spaces = ["words","chars"]
+            question_df = DataSet.load_all_questions()
+            all_qids = DataSet.load_all_unique_ids_train_test()
+            for space in spaces:
+                print("for",space)
+                corpus = question_df[space]
+                w2i,i2w = self._get_item2id_id2item(corpus)
+                self.item2idx[space] = w2i
+                self.idx2item[space] = i2w
+                ##Finish mapping table
+                term_embed = DataSet.load_term_embed(space)
+                embed_size = term_embed.shape[1]
+                pad_embed = np.array([0] * embed_size).reshape(1, -1)
+                all_embeding = np.vstack([pad_embed, term_embed])
+                all_index = [_PAD_] + term_embed.index.values.tolist()
+                all_embeding_df = pd.DataFrame(data=all_embeding, index=all_index)
+                sort_word = [i2w[i] for i in range(len(i2w))]
+                self.item_embed[space] = all_embeding_df.loc[sort_word].values
+                ##Finish item embedding
+                tmp_q2idvec = {}
+                for qid in all_qids:
+                    items = question_df.loc[qid][space].split()
+                    idvec = np.array([w2i[w] for w in items])
+                    tmp_q2idvec[qid] = idvec
+                self.q2idvec[space]=tmp_q2idvec
+                ##Finish map from question to id vector
+            print("finish generating inter files.")
+            print("begin caching..")
+            all_cached = {}
+            all_cached["item2idx"] =  self.item2idx
+            all_cached["idx2item"] =  self.idx2item
+            all_cached["item_embed"] = self.item_embed
+            all_cached["q2idvec"] = self.q2idvec
+            try:
+                os.makedirs("./temp")
+            except:
+                pass
+            pickle.dump(all_cached,open(self._temp_file,"wb"))
+            print("finish caching")
+
+    def prepare(self,data_df,bucket_num,space,is_prefix_pad,is_test):
+        print("prepare necessary data...")
+        assert space in ["words","chars"]
+        self.is_test = is_test
+        self.data_df = data_df
+        q_len_title = ["%s_len_q%s"%(space[:-1],i) for i in [1,2]]
+        q_pair_list = list(zip(data_df[q_len_title[0]],
+                               data_df[q_len_title[1]]
+                               )
+                           )
+        bucket = GreedyBucket()
+        fit_res = bucket.fit(q_pair_list)
+        buckets,bounds = bucket.get_split_results(fit_res,bucket_num)
+        self.buckets= buckets
+        self.bounds = bounds
+        data_set_id_vectors = []
+        q2idvs = self.q2idvec[space]
+        print(len(self.data_df))
+        for ind in xrange(len(self.data_df)):
+            cur_row = data_df.iloc[ind]
+            cur_q1 = cur_row["q1"]
+            cur_q2 = cur_row["q2"]
+            q1_idv = q2idvs[cur_q1]
+            q2_idv = q2idvs[cur_q2]
+            cur_bound = bounds[ind]
+            q1_pad_len = cur_bound - len(q1_idv)
+            q2_pad_len = cur_bound - len(q2_idv)
+            if is_prefix_pad:
+                cur_q1_padded = np.pad(q1_idv,(q1_pad_len,0),"constant")
+                cur_q2_padded = np.pad(q2_idv,(q2_pad_len,0),"constant")
+            else:
+                cur_q1_padded = np.pad(q1_idv,(0,q1_pad_len),"constant")
+                cur_q2_padded = np.pad(q2_idv,(0,q2_pad_len),"constant")
+            cur_pair_padded = np.concatenate([cur_q1_padded,cur_q2_padded])
+            data_set_id_vectors.append(cur_pair_padded)
+        self.data_set_id_vectors = np.array(data_set_id_vectors)
+        print("prepare done.")
+
+    def get_data_generator(self,is_shuffle,batch_size):
+        bucketkeys = list(self.buckets.keys())
+        if is_shuffle:
+            np.random.shuffle(bucketkeys)
+        else:
+            bucketkeys.sort() ##if not shuffle, always starts with the bucket with smallest length
+        for b in bucketkeys:
+            id_list = self.buckets[b]
+            tmpdata = np.vstack(self.data_set_id_vectors[id_list])
+            if not self.is_test:
+                tmplabel = self.data_df["label"].iloc[id_list].values
+            tmpids = np.array(id_list)
+            tmpsels = np.arange(len(tmpids))
+            if is_shuffle:
+                np.random.shuffle(tmpsels)
+            if len(tmpids)%batch_size == 0:
+                nums = len(tmpids)/batch_size
+            else:
+                nums = len(tmpids)/batch_size + 1
+            for time in xrange(nums):
+                start = time*batch_size
+                end = (time+1)*batch_size
+                sel = tmpsels[start:end].tolist()
+                if not self.is_test:
+                    yield torch.LongTensor(tmpdata[sel]),torch.LongTensor(tmpids[sel]),torch.LongTensor(tmplabel[sel])
+                    #yield tmpdata[sel],tmplabel[sel],tmpids[sel]
+                else:
+                    yield torch.LongTensor(tmpdata[sel]), torch.LongTensor(tmpids[sel])
+                    #yield tmpdata[sel], tmpids[sel]
+
+    def _get_item2id_id2item(self,corpus):
+        item2idx = {_PAD_:0}
+        sen_list = corpus.values.tolist()
+        for sen in sen_list:
+            for word in sen.split():
+                if word not in item2idx:
+                    item2idx[word] = len(item2idx)
+        idx2item = {v:k for k,v in item2idx.items()}
+        return item2idx,idx2item
+
+    @property
+    def _temp_file(self):
+        combine_str = "all_inter_files_item2idx_idx2item_item_embed_q2idvec"
+        hash_object = hashlib.md5(combine_str.encode("utf-8"))
+        hexcode = hash_object.hexdigest()
+        return "./temp/%s_%s.pkl"%(combine_str,hexcode)
+
+    def get_item_embed_tensor(self,space):
+        return torch.Tensor(self.item_embed[space])
+
+
 
 class MyDataLoader(object):
     def __init__(self,bucket_num,batch_size,train_test,data_space,shuffle=True,pad_prefix=True):
@@ -264,8 +440,14 @@ if __name__ == "__main__":
     # fitres = bucket.fit(a)
     # bucket,bounds = bucket.get_split_results(fitres,5)
     # print bucket,bounds
-    dl = MyDataLoader(bucket_num=5,batch_size=2,train_test="train",data_space="words")
-    dit = dl.get_data_iterator()
-    for d,y in dit:
+    dl = DataGenerator()
+    train = DataSet.load_train()
+    from sklearn.model_selection import train_test_split
+
+    xtr, xte = train_test_split(train, test_size=0.33)
+    dl.prepare(xtr[:10], 1, "words", True, False)
+    tr_g = dl.get_data_generator(True, 20)
+    for d,i,l in tr_g:
         print(d)
-        print(y)
+        print(i)
+        print(l)
--- scripts/Siamese_CNN_v1/model.py
+++ scripts/Siamese_CNN_v1/model.py
@@ -8,8 +8,8 @@ from torch.nn import functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 class Siamese_CNN(nn.Module):
-    def __init__(self,list_filter_size,if_tune_embedding,pre_trained_embedding):
+    def __init__(self,pre_trained_embedding,is_freeze):
         super(Siamese_CNN,self).__init__()
-        self.list_filter_size = list_filter_size
-        self.if_tune_embedding = if_tune_embedding
+        self.nn_Embedding = nn.Embedding.from_pretrained(pre_trained_embedding,freeze=is_freeze)
+
 
